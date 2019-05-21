6000e6593ea8c572f6d05864092e62eb05b22676
Alexandre Mulatinho <alex@mulatinho.net>
2019-05-21 15:25:29 -0300

    changing from tab to spaces

--- config/config.js
+++ config/config.js
@@ -1,27 +1,27 @@
 /*
-	Todo-App RESTful API
-	(c) 2019 Alexandre Mulatinho
+  Todo-App RESTful API
+  (c) 2019 Alexandre Mulatinho
 */
 
 const env  = process.env.NODE_ENV || "development";
 const path = require('path')
 
 var config = {
-	development: {
-		web: { port: 4000 },
-		database: {
-			name: 'todo_dev',
-			options: {
-				host: 'localhost',
-				dialect: 'sqlite',
-				storage: __dirname + '/../data/todo_dev.sqlite3',
-				pool: { max: 5, min: 1, idle: 10000 }
-			}
-		}
-	},
-	test: {
+  development: {
+    web: { port: 4000 },
+    database: {
+      name: 'todo_dev',
+      options: {
+        host: 'localhost',
+        dialect: 'sqlite',
+        storage: __dirname + '/../data/todo_dev.sqlite3',
+        pool: { max: 5, min: 1, idle: 10000 }
+      }
+    }
+  },
+  test: {
 
-	}
+  }
 };
 
 module.exports = config[env];
