749168caccab45f453c9b06df50495ebf6fd03a9
Alexandre Mulatinho <alex@mulatinho.net>
2019-05-23 10:07:22 -0300

    making authentication work on todo-app

--- index.js
+++ index.js
@@ -45,22 +45,6 @@ function configure(app) {
   app.set('views', './views');
   app.set('view engine', 'pug');
   app.disable('x-powered-by');
-
-  if ('production' == app.get('env') || 'production' == process.env.NODE_ENV) {
-    // production error handler, no stacktraces leaked to user
-    app.use(function(error, request, response, next) {
-      console.log('logging events and showing stacktrace only local..')
-      console.log('errorStatus: ', error.status, 'errorMessage: ', error.message);
-      response.status(500).json({});
-    })
-  } else {
-    // handle errors and print stacktrace
-    app.use(function(error, request, response, next) {
-      console.log('logging events and showing stacktrace..');
-      console.log('message: ' + error.message + '\nerror: ' + error);
-      response.status(error.status || 500).json({ 'message': error.message, 'error': error });
-    })
-  }
 }
 
 //
@@ -73,9 +57,7 @@ configure(app);
 routeApi(app, sequelize);
 
 // synchronize data, remaking the tables if neeeded.
-db.sequelize.authenticate({
-  // timezone: '-03:00'
-}).then(function () {
+db.sequelize.sync({}).then(function () {
   const port = config.web.port;
 
   if (!module.parent) {
@@ -87,5 +69,21 @@ db.sequelize.authenticate({
 
 // render 404 view if user browse something unexpected.
 app.use(function(req, res) { res.status(404).render('404'); });
+
+if ('production' == app.get('env') || 'production' == process.env.NODE_ENV) {
+  // production error handler, no stacktraces leaked to user
+  app.use(function(error, request, response, next) {
+    console.log('logging events and showing stacktrace only local..')
+    console.log('errorStatus: ', error.status, 'errorMessage: ', error.message);
+    response.status(500).json({});
+  })
+} else {
+  // handle errors and print stacktrace
+  app.use(function(error, request, response, next) {
+    console.log('logging events and showing stacktrace..');
+    console.log('message: ' + error.message + '\nerror: ' + error);
+    response.status(error.status || 500).json({ 'message': error.message, 'error': error });
+  })
+}
 
 module.exports = app;
